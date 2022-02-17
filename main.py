e1c9b56e0f737a0897538303e1b720eada8f8fdd
jamesrivett <jrivett@students.kennesaw.edu>
2022-02-17 15:40:46 -0500

    import data set

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+
+raw_data = pd.read_csv('BTC-USD-2021.csv')
+raw_data['DateTime'] = pd.to_datetime('Date')
+print(raw_data)
