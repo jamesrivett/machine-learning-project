1e3c1a53b62d27f6d4d876ae90732c0252fbe7d0
jamesrivett <jrivett@students.kennesaw.edu>
2022-03-14 19:15:27 -0400

    production push 1.0

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 # Load Libraries
-from typing import final
+from appJar import gui
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
+import datetime as dt
 from copy import deepcopy
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
-
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 
-
 # Globals
-# API
+# REQUEST URL Vars, These are default values. The UI will change them before making the reuquest.
 global COIN_SYMBOL; COIN_SYMBOL = 'BTC'
 global PERIOD_ID; PERIOD_ID = '1DAY'
 global START_DATE; START_DATE = '2021-01-01'
@@ -28,6 +27,8 @@ global REQUEST_URL; REQUEST_URL = 'https://rest.coinapi.io/v1/exchangerate/{}/US
 global LOOK_BACK; LOOK_BACK = 10
 global NUM_EPOCHS; NUM_EPOCHS = 200
 
+app = gui("ML Final Project", " 800x900")
+
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, lookBack=1):
   dataX, dataY = [], []
@@ -37,73 +38,123 @@ def create_dataset(dataset, lookBack=1):
     dataY.append(dataset[i + lookBack, 0])
   return np.array(dataX), np.array(dataY)
 
-def doPrediction(coinSymbol, periodID, startDate, endDate, limit, apiKey):
-    # Import BTC/USD data
-    url = REQUEST_URL.format(coinSymbol, periodID, startDate, endDate, limit, apiKey)
-    data = pd.read_csv(url, sep=';')
-    print(data)
+def getPeriod(period):
+  values = {"1 Hour": "1HR", "12 hour": "12HRS", "Day": "1DAY", "Week": "7DAY"}
+  return values[period]
 
-    # Create Dataframe
-    df = deepcopy(data)
-    df['datetime'] = pd.to_datetime(df['time_period_start'])
-    df = df[['rate_open']]
-    dataset = df.values
-    dataset = dataset.astype('float32')
-    print(dataset)
+def getSymbol(symbol):
+  values = {"Bitcoin": "BTC",  "Ethereum": "ETH", "Tether": "ETH", "ADA": "ADA",  "DogeCoin": "DOGE", "AVAX":  "AVAX",  "XTZ": "XTZ",  "ShibaCoin": "SHIB",  "DOT": "DOT", "SOL": "SOL"}
 
-    # Scale Data Frame
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    dataset = scaler.fit_transform(dataset)
+def doPrediction(btn):
+  # Harvest Inputs
+  StartingDay = app.getEntry("Starting Day:    ")
+  StartingMonth = app.getEntry("Starting Month: ")
+  StartingYear = app.getEntry("Starting Year:   ")
+  EndingDay = app.getEntry("Ending Day:    ")
+  EndingMonth = app.getEntry("Ending Month: ")
+  EndingYear = app.getEntry("Ending Year:   ")
+  period = app.getOptionBox("Trading Interval")
+  symbol = app.getOptionBox("Select a Crypto")
 
-    # Define Training and Testing Boundaries
-    train_size = int(len(dataset) * 0.67)
-    test_size = len(dataset) - train_size
-    train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+  # Build URL Variables
+  CON_SYMBOL = getSymbol(symbol)
+  PERIOD_ID = getPeriod(period)
+  START_DATE = dt.datetime(int(StartingYear), int(StartingMonth), int(StartingDay))
+  END_DATE = dt.datetime(int(EndingYear), int(EndingMonth), int(EndingDay))
 
-    # Create Data Sets
-    trainX, trainY = create_dataset(train, lookBack=LOOK_BACK)
-    testX, testY = create_dataset(test, lookBack=LOOK_BACK)
+  
 
-    # Reshape input to be [samples, time steps, features]
-    trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-    testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+  # Import BTC/USD data
+  url = REQUEST_URL.format(COIN_SYMBOL, PERIOD_ID, str(START_DATE.date()), str(END_DATE.date()), LIMIT, API_KEY)
+  print(url)
+  data = pd.read_csv(url, sep=';')
+  print(data)
 
-    # Build Model
-    model = Sequential()
-    model.add(LSTM(4, input_shape=(1, LOOK_BACK)))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(trainX, trainY, epochs=NUM_EPOCHS, batch_size=256, verbose=2)
+  # Create Dataframe
+  df = deepcopy(data)
+  df['datetime'] = pd.to_datetime(df['time_period_start'])
+  df = df[['rate_open']]
+  dataset = df.values
+  dataset = dataset.astype('float32')
+  print(dataset)
 
-    # Make Predictions
-    trainPredict = model.predict(trainX)
-    testPredict = model.predict(testX)
+  # Scale Data Frame
+  scaler = MinMaxScaler(feature_range=(0, 1))
+  dataset = scaler.fit_transform(dataset)
 
-    # Revert Predictions to Previous Scale
-    trainPredict = scaler.inverse_transform(trainPredict)
-    trainY = scaler.inverse_transform([trainY])
-    testPredict = scaler.inverse_transform(testPredict)
-    testY = scaler.inverse_transform([testY])
+  # Define Training and Testing Boundaries
+  train_size = int(len(dataset) * 0.67)
+  test_size = len(dataset) - train_size
+  train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
 
-    # Print Scores
-    trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
-    print('Train Score: %.2f RMSE' % (trainScore))
-    testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
-    print('Test Score: %.2f RMSE' % (testScore))
+  # Create Data Sets
+  trainX, trainY = create_dataset(train, lookBack=LOOK_BACK)
+  testX, testY = create_dataset(test, lookBack=LOOK_BACK)
 
-    # Plot Results
-    trainPredictPlot = np.empty_like(dataset)
-    trainPredictPlot[:, :] = np.nan
-    trainPredictPlot[LOOK_BACK:len(trainPredict) + LOOK_BACK, :] = trainPredict
-    testPredictPlot = np.empty_like(dataset)
-    testPredictPlot[:, :] = np.nan
-    testPredictPlot[len(trainPredict) + (LOOK_BACK * 2) + 1:len(dataset) - 1, :] = testPredict
-    plt.plot(df['rate_open'], label='Actual')
-    plt.plot(pd.DataFrame(trainPredictPlot, columns=["rate_close"], index=df.index).rate_close, label='Training')
-    plt.plot(pd.DataFrame(testPredictPlot, columns=["rate_close"], index=df.index).rate_close, label='Testing')
-    plt.legend(loc='best')
-    plt.show()
+  # Reshape input to be [samples, time steps, features]
+  trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+  testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 
+  # Build Model
+  model = Sequential()
+  model.add(LSTM(4, input_shape=(1, LOOK_BACK)))
+  model.add(Dense(1))
+  model.compile(loss='mean_squared_error', optimizer='adam')
+  model.fit(trainX, trainY, epochs=NUM_EPOCHS, batch_size=256, verbose=2)
+
+  # Make Predictions
+  trainPredict = model.predict(trainX)
+  testPredict = model.predict(testX)
+
+  # Revert Predictions to Previous Scale
+  trainPredict = scaler.inverse_transform(trainPredict)
+  trainY = scaler.inverse_transform([trainY])
+  testPredict = scaler.inverse_transform(testPredict)
+  testY = scaler.inverse_transform([testY])
+
+  # Print Scores
+  trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
+  print('Train Score: %.2f RMSE' % (trainScore))
+  testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
+  print('Test Score: %.2f RMSE' % (testScore))
+
+  # Plot Results
+  trainPredictPlot = np.empty_like(dataset)
+  trainPredictPlot[:, :] = np.nan
+  trainPredictPlot[LOOK_BACK:len(trainPredict) + LOOK_BACK, :] = trainPredict
+  testPredictPlot = np.empty_like(dataset)
+  testPredictPlot[:, :] = np.nan
+  testPredictPlot[len(trainPredict) + (LOOK_BACK * 2) + 1:len(dataset) - 1, :] = testPredict
+  plt.plot(df['rate_open'], label='Actual')
+  plt.plot(pd.DataFrame(trainPredictPlot, columns=["rate_close"], index=df.index).rate_close, label='Training')
+  plt.plot(pd.DataFrame(testPredictPlot, columns=["rate_close"], index=df.index).rate_close, label='Testing')
+  plt.legend(loc='best')
+  plt.show()
+
+
+def main():
+    # Build UI
+    app.setFont(20)
+    app.addLabel("l0", "ML interface")
+    app.setLabelBg("l0", "blue")
+    app.addLabelOptionBox("Select a Crypto",["   - Cryptocurrencies -", "Bitcoin", "Ethereum",
+                            "Tether", "ADA", "DogeCoin", "AVAX", "XTZ",
+                            "ShibaCoin", "DOT","SOL"])
+    app.startLabelFrame("Timeframe for prediction")
+    app.setFont(20)
+    app.addLabelEntry("Starting Day:    ")
+    app.addLabelEntry("Starting Month: ")
+    app.addLabelEntry("Starting Year:   ")
+    app.setFont(14)
+    app.addLabel("a", " ")
+    app.addLabelEntry("Ending Day:    ")
+    app.addLabelEntry("Ending Month: ")
+    app.addLabelEntry("Ending Year:   ")
+    app.addLabelOptionBox("Trading Interval", ["1 Hour", "12 hour", "Day", "Week"])
+    app.addButton("Predict", doPrediction)
+    app.setFont(20)
+
+    app.go()
 
 if __name__ == "__main__":
-    doPrediction(COIN_SYMBOL, PERIOD_ID, START_DATE, END_DATE, LIMIT, API_KEY)
+    main()
